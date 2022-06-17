165dac6e7127f46e73fe4ba705393c39da3e35cc
kramstyles <mark.eke@decagon.dev>
2022-06-17 10:03:43 +0100

    added axios reset route

--- src/pages/Auth/PasswordResetFour/index.jsx
+++ src/pages/Auth/PasswordResetFour/index.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import { Text, Button, Input } from 'components';
+import { useParams } from 'react-router-dom';
 
-export const PasswordResetFour = () => (
-  <div className="wrapper-container">
-    <Text.Heading text="Reset Password" size={24} weight={500} level={1} />
+export const PasswordResetFour = () => {
+  const params = useParams();
+  const encodedEmail = params['*'];
 
-    <div className="wrapper--email">
-      <div className="wrapper--label--email">
-        <Text text="Enter OTP" />
+  console.log(encodedEmail);
+
+  return (
+    <div className="wrapper-container">
+      <Text.Heading text="Reset Password" size={24} weight={500} level={1} />
+
+      <div className="wrapper--email">
+        <div className="wrapper--label--email">
+          <Text text="Enter OTP" />
+        </div>
+        <div className="wrapper--input--email">
+          <Input.FullRound name="otp" type="text" placeholder="1234" id="txtOtp" />
+        </div>
       </div>
-      <div className="wrapper--input--email">
-        <Input.FullRound name="otp" type="text" placeholder="1234" id="txtOtp" />
+      <div className="wrapper--email">
+        <div className="wrapper--label--email">
+          <Text text="Enter your new Password" />
+        </div>
+        <div className="wrapper--input--email">
+          <Input.FullRound name="password" type="password" placeholder="Abc123." id="txtPassword" />
+        </div>
       </div>
+      <div className="wrapper--email">
+        <div className="wrapper--label--email">
+          <Text text="Re-type your new Password" />
+        </div>
+        <div className="wrapper--input--email">
+          <Input.FullRound name="confirmPassword" type="password" placeholder="Abc123." id="txtConfirm" />
+        </div>
+      </div>
+      <Button stretch btnType="primary" text="Reset Password" />
     </div>
-    <div className="wrapper--email">
-      <div className="wrapper--label--email">
-        <Text text="Enter your new Password" />
-      </div>
-      <div className="wrapper--input--email">
-        <Input.FullRound name="password" type="password" placeholder="Abc123." id="txtPassword" />
-      </div>
-    </div>
-    <div className="wrapper--email">
-      <div className="wrapper--label--email">
-        <Text text="Re-type your new Password" />
-      </div>
-      <div className="wrapper--input--email">
-        <Input.FullRound name="confirmPassword" type="password" placeholder="Abc123." id="txtConfirm" />
-      </div>
-    </div>
-    <Button stretch btnType="primary" text="Reset Password" />
-  </div>
-);
+  );
+};
