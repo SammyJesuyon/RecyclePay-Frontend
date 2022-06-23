944d23a3aaaf74893b26f1dea4482099f2787718
SammyJesuyon <sammyjesuyon@yahoo.com>
2022-06-23 10:16:40 +0100

    signuppage implemented to show email verified

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DashHeader, SideBar, Text, RecentPickups, Welcome } from 'components';
 import './index.css';
 import { pickups } from 'components/molecules/RecentPickups/pickups';
+import routes from 'apis/routes';
+// import { userData } from './dummy';
 
-export const OverviewPage = () => (
-  <div className="overview">
-    <div className="sidebar">
-      <SideBar />
-    </div>
-    <div className="content">
-      <div className="header">
-        <DashHeader />
+export const OverviewPage = () => {
+  const [userActivities, setUserActivities] = useState('');
+  const getUserActivities = async () => {
+    const res = await routes.dashboard.main();
+    setUserActivities(res.data);
+    console.log('dashboard-data', userActivities);
+  };
+  useEffect(() => {
+    getUserActivities();
+  }, []);
+  return (
+    <div className="overview">
+      <div className="sidebar">
+        <SideBar />
       </div>
-      <div className="welcome">
-        <Welcome />
-      </div>
-      <div className="head-text">
-        <Text>Recent Pickups</Text>
-        <div className="right-text">
-          <Text>Your Total Points</Text>
-          &nbsp;1769
+      <div className="content">
+        <div className="header">
+          <DashHeader />
+        </div>
+        <div className="welcome">
+          <Welcome />
+        </div>
+        <div className="head-text">
+          <Text>Recent Pickups</Text>
+          <div className="right-text">
+            <Text>Your Total Points</Text>
+            &nbsp;1769
+          </div>
+        </div>
+        <div>
+          {pickups.map((item) => (
+            <RecentPickups {...item} key={item.id} />
+          ))}
         </div>
       </div>
-      <div>
-        {pickups.map((item) => (
-          <RecentPickups {...item} key={item.id} />
-        ))}
-      </div>
     </div>
-  </div>
-);
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { SignUpPage } from 'pages/Auth';
 export { SignUpPage2 } from 'pages/Auth';
+export { SignUpPage3 } from 'pages/Auth';
 export { SignInPage } from 'pages/Auth';
 export { PasswordResetOne } from 'pages/Auth';
 export { PasswordResetTwo } from 'pages/Auth';
